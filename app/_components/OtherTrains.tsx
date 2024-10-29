7ea6ff56a5da6e246e321254016ab65a848d11a4
Maximus019BG <maxralev@abv.bg>
2024-10-30 00:01:46 +0200

    chore:final commit

--- app/_components/OtherTrains.tsx
+++ app/_components/OtherTrains.tsx
@@ -4,9 +4,11 @@ import React from "react";
 const OtherTrains = () => {
   return (
     <div className="flex justify-center items-center w-full -mt-16 py-6">
-      <button className="z-30 mt-10 bg-teal-800 px-12 text-white py-2 rounded-lg font-semibold hover:bg-teal-900">
-        <Link href="/trains"> See other trains </Link>
-      </button>
+      <Link href="/trains">
+        <button className="z-30 mt-10 bg-teal-800 px-12 text-white py-2 rounded-lg font-semibold hover:bg-teal-900">
+          See other trains
+        </button>
+      </Link>
     </div>
   );
 };
